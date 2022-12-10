f39240a020473690f0532f4df9974a09ebbf3011
nobnap <m.j.d.f.r.2003@gmail.com>
2022-12-10 19:15:03 +0000

    Formatted the code

--- solution1.cpp
+++ solution1.cpp
@@ -20,37 +20,37 @@ bool isEmpty(vector<int> &staircase) {
 }
 
 void print_stairs(vector<int> &staircase) {
-    for (vector<int>::iterator it = staircase.begin(); it != staircase.end(); it++) 
-        cout << *it << endl;
+	for (vector<int>::iterator it = staircase.begin(); it != staircase.end(); it++) 
+		cout << *it << endl;
 }
 
 vector<int>::iterator get_max(vector<int> &staircase) { 
-    vector<int>::iterator itr = staircase.begin();
-    vector<int>::iterator max_line = itr;
+	vector<int>::iterator itr = staircase.begin();
+	vector<int>::iterator max_line = itr;
 
-    itr++;
+	itr++;
 
-    for (; itr != staircase.end(); itr++) {
+	for (; itr != staircase.end(); itr++) {
 		if (*itr > *max_line) {
-            max_line = itr;
-        }
+			max_line = itr;
+		}
 	}
-    return max_line;
+	return max_line;
 }
 
 int max_block(vector<int> &staircase) { // TODO e se só tiver 0?
-    vector<int>::iterator max_line = get_max(staircase);
-    int max_row = 0;
-    int block = 0;
+	vector<int>::iterator max_line = get_max(staircase);
+	int max_row = 0;
+	int block = 0;
 
-    for (vector<int>::iterator itr = max_line; *itr == *max_line && itr != staircase.end(); itr++) {
-        max_row++; 
+	for (vector<int>::iterator itr = max_line; *itr == *max_line && itr != staircase.end(); itr++) {
+		max_row++; 
 	}
 
-    // compara blocos na horizontal(max_line) que na vertical(max_row) e devolve o menor entre ambos
-    block = *max_line > max_row ? max_row : *max_line; 
+	// compara blocos na horizontal(max_line) que na vertical(max_row) e devolve o menor entre ambos
+	block = *max_line > max_row ? max_row : *max_line; 
 
-    return block;
+	return block;
 }
 
 void remove_block(vector<int>::iterator itr, int num) {
@@ -67,62 +67,62 @@ void add_block(vector<int>::iterator itr, int num) {
 	}
 }
 
-    size_t hasher(vector<int> const& lst) {
-        size_t seed = lst.size();
-        for(auto x : lst) {
-            x = ((x >> 16) ^ x) * 0x45d9f3b;
-            x = ((x >> 16) ^ x) * 0x45d9f3b;
-            x = (x >> 16) ^ x;
-            seed ^= x + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-        }
-        return seed;
-    }
+size_t hasher(vector<int> const& lst) {
+	size_t seed = lst.size();
+	for(auto x : lst) {
+		x = ((x >> 16) ^ x) * 0x45d9f3b;
+		x = ((x >> 16) ^ x) * 0x45d9f3b;
+		x = (x >> 16) ^ x;
+		seed ^= x + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+	}
+	return seed;
+}
 
 unsigned long long fill_staircase(vector<int> &staircase, map<int, unsigned long long> &memoization) { 
 
 	if (isEmpty(staircase)) return 1; // TODO maybe check size and if there's only 1 line or row return aswell
 
-    if (memoization.count(hasher(staircase))) {
-        return memoization[hasher(staircase)];
-    }
+	if (memoization.count(hasher(staircase))) {
+		return memoization[hasher(staircase)];
+	}
 
-    vector<int>::iterator max_line_itr = get_max(staircase);
-    int max_tile = max_block(staircase);
+	vector<int>::iterator max_line_itr = get_max(staircase);
+	int max_tile = max_block(staircase);
 
 	unsigned long long options = 0;
 	for (int i=0; i < max_tile; i++) {
 
 		remove_block(max_line_itr, i+1); // FIXME
 		options += fill_staircase(staircase, memoization);
-        add_block(max_line_itr, i+1);
+		add_block(max_line_itr, i+1);
 	}
 
-    memoization.insert({hasher(staircase), options});
+	memoization.insert({hasher(staircase), options});
 
 	return options;
 }
 
 int main() {
-    map<int, unsigned long long> memoization;
-    vector<int> staircase;
+	map<int, unsigned long long> memoization;
+	vector<int> staircase;
 
-    int size_x; 
-    int size_y;
-    int counter;
-    int stair;
+	int size_x; 
+	int size_y;
+	int counter;
+	int stair;
 
-    cin >> size_y;
-    cin >> size_x;
-    
-    counter = size_y;
-    while (cin >> stair && counter) {
-        staircase.push_back(stair);
-        counter--;
-    }
-    
-    unsigned long long result = !isEmpty(staircase)? fill_staircase(staircase, memoization) : 0;
-    cout << result << endl;
+	cin >> size_y;
+	cin >> size_x;
+	
+	counter = size_y;
+	while (cin >> stair && counter) {
+		staircase.push_back(stair);
+		counter--;
+	}
+	
+	unsigned long long result = !isEmpty(staircase)? fill_staircase(staircase, memoization) : 0;
+	cout << result << endl;
 
-    return 0;
+	return 0;
 }
 
